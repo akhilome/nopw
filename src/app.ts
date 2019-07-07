b8d806c3ae29af7006ea2a3562076274e22d01a0
Kizito Akhilome <hovkard@gmail.com>
2019-07-07 17:01:51 +0100

    ch: setup base project config with ts (#1)

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'server up & running'
+  });
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
